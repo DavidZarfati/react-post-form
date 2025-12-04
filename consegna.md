bdd630d3fbb0019eda778e5933e963a978437b35
DavidZarfati <zarda2001@gmail.com>
2025-12-04 16:25:28 +0100

    feat:creata card e aggiunta lista con le card

---
+++ consegna.md
@@ -0,0 +1,29 @@
+ 
+ Ciao ragazzi,
+ esercizio di oggi:  Post Form
+ 
+ repo: react-post-form
+ 
+ Oggi creeremo il nostro primo form multifield per poter elaborare i dati. 
+ 
+ Dovremo creare una nuova app React che contenga un form per creare un nuovo post all’interno di un blog. 
+ 
+ I dati che il form dovrà richiedere sono i seguenti:
+ 
+ 
+author (string) - L’autore del post
+title (string) - Il titolo del post
+body (string) - Il testo del post
+public (boolean) - Se il post deve essere pubblico (true) o una bozza (false)
+
+All'submit del form, dovete aggiungere il nuovo post all'array dei post e mostrarlo in pagina
+ 
+ Bonus
+ 
+ 
+aggiungere e gestire un alert di conferma al submit del form
+eseguire dei controlli di vari input
+aggiungere altri input a vostro piacere
+individuare qualche componente
+
+Buon Lavoro 💪
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,100 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css'
 
+const initialFormData = {
+  author: "",
+  title: "",
+  body: "",
+  pubblico: "",
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  // const [author, setauthor] = useState("")
+  // const [title, setTitle] = useState("")
+  // const [body, setBody] = useState("")
+  // const [pubblico, setPubblico] = useState("")
+  const [cards, setCards] = useState([])
+  const [formData, setFormData] = useState(initialFormData)
+  function updateFormData(event) {
+    const key = event.target.name
+    const newObject = {
+      ...formData,
+      [key]: event.target.value
+    }
+    setFormData(newObject)
+  }
+  function GestisciClick(event) {
+    event.preventDefault();
+    setCards([...cards, formData]);
+    setFormData(initialFormData); // reset form
+  }
 
   return (
     <>
+      <div className="container">
+        <div className="row">
 
+          <div className="col-12 mb-2">
+            <div className="row">
+
+              <div className="col-3 mb-2">
+                <label htmlFor="author">author</label>
+                <input type="text"
+                  id='author'
+                  name='author'
+                  className='form-control'
+                  value={formData.author}
+                  onChange={updateFormData} />
+              </div>
+              <div className="col-3 mb-2">
+                <label htmlFor="title">title</label>
+                <input type="text"
+                  id='title'
+                  name='title'
+                  className='form-control'
+                  value={formData.title}
+                  onChange={updateFormData} />
+              </div>
+              <div className="col-6 mb-2">
+                <label htmlFor="body">body of the article</label>
+                <input type="text"
+                  id='body'
+                  name='body'
+                  className='form-control'
+                  value={formData.body}
+                  onChange={updateFormData} />
+              </div>
+
+
+              <div className="col-12">
+                <button onClick={GestisciClick}>Invia</button>
+              </div>
+
+              {cards.length > 0 && cards.map((card, index) => (
+                <div className="col-12 col-4" key={index}>
+                  <div className="card">
+                    <div className="card-header">{card.title} written by: {card.author}</div>
+                    <div className="card-body">
+                      <p>{card.body}</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+
+
+
+
+            </div>
+          </div>
+
+
+
+
+
+
+        </div>
+      </div>
     </>
   )
 }
